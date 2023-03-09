a35d54c36026c7864b6c0c2504c26669be942bb0
kaser137 <107580630+kaser137@users.noreply.github.com>
2023-03-09 07:45:11 +0200

    Add verbouse names.

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,11 +3,11 @@ from django.db import models
 
 class Pokemon(models.Model):
     id = models.IntegerField(primary_key=True)
-    title = models.CharField(verbose_name='Название', max_length=200)
-    photo = models.ImageField(upload_to='photo', null=True, blank=True)
+    title = models.CharField('Название', max_length=200)
+    photo = models.ImageField('Изображение', upload_to='photo', null=True, blank=True)
     description = models.TextField('Описание', null=True, blank=True)
-    title_en = models.CharField(verbose_name='Название на английском', max_length=200, null=True, blank=True)
-    title_jp = models.CharField(verbose_name='Название на японском', max_length=200, null=True, blank=True)
+    title_en = models.CharField('Название на английском', max_length=200, null=True, blank=True)
+    title_jp = models.CharField('Название на японском', max_length=200, null=True, blank=True)
     next_evolution = models.ForeignKey('self', verbose_name='В кого эволюционирует', on_delete=models.SET_NULL, related_name='next_for', null=True, blank=True)
     previous_evolution = models.ForeignKey('self', verbose_name='Из кого эволюционирует', on_delete=models.SET_NULL, null=True,
                                        blank=True, related_name='previous_for')
@@ -19,14 +19,14 @@ class Pokemon(models.Model):
 
 class PokemonEntity(models.Model):
     pokemon = models.ForeignKey(Pokemon, related_name='entity', on_delete=models.CASCADE)
-    lat = models.FloatField(blank=True)
-    lon = models.FloatField(blank=True)
-    appeared_at = models.DateTimeField(null=True, blank=True)
-    disappeared_at = models.DateTimeField(null=True, blank=True)
-    level = models.IntegerField(null=True, blank=True)
-    health = models.IntegerField(null=True, blank=True)
-    strength = models.IntegerField(null=True, blank=True)
-    defence = models.IntegerField(null=True, blank=True)
-    stamina = models.IntegerField(null=True, blank=True)
+    lat = models.FloatField("Широта", blank=True)
+    lon = models.FloatField("Долгота", blank=True)
+    appeared_at = models.DateTimeField("Появление", null=True, blank=True)
+    disappeared_at = models.DateTimeField("Исчезновение", null=True, blank=True)
+    level = models.IntegerField("Уровень", null=True, blank=True)
+    health = models.IntegerField("Здоровье", null=True, blank=True)
+    strength = models.IntegerField("Сила", null=True, blank=True)
+    defence = models.IntegerField("Защита", null=True, blank=True)
+    stamina = models.IntegerField("Энергия", null=True, blank=True)
     def __str__(self):
         return 'pokemon entity'
